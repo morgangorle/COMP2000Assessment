0ee995f855b9c89daf20125d4eafe3190769204f
deskara <55700741+deskara@users.noreply.github.com>
2020-12-31 16:23:41 +0000

    Added an entry-point into the program
    Also added some test data in CSV format.

--- MainAssignment/src/MainGUI.java
+++ MainAssignment/src/MainGUI.java
@@ -1,2 +1,25 @@
-public class MainGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainGUI extends JFrame {
+    private JPanel MainPanel;
+
+    public MainGUI(){
+        setContentPane(MainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500,500));
+        pack();
+
+    }
+    public static void main(String[] args){
+
+        MainGUI page = new MainGUI();
+        page.setVisible(true);
+
+
+
+
+
+
+    }
 }
