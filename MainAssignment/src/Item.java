3e10cb937ea16f7daafed04147bd064acf4f8677
deskara <55700741+deskara@users.noreply.github.com>
2021-01-11 17:04:35 +0000

    Added an Item class
    This will be used for items in the check-out system.

---
+++ MainAssignment/src/Item.java
@@ -0,0 +1,36 @@
+public class Item {
+    private String Name;
+    private String Code;
+    private float Price;
+    private int AmountInStock;
+    public Item(String inputName, String inputCode,float inputPrice,int inputStock){
+        Name = inputName;
+        Code = inputCode;
+        Price = inputPrice;
+        AmountInStock = inputStock;
+    }
+    public String getName(){
+        return Name;
+    }
+    public String getCode(){
+        return Code;
+    }
+    public float getPrice(){
+        return Price;
+    }
+    public int getAmountInStock(){
+        return AmountInStock;
+    }
+    public void setName(String inputName){
+        Name = inputName;
+    }
+    public void setCode(String inputCode){
+        Code = inputCode;
+    }
+    public void setPrice(Float inputPrice){
+        Price = inputPrice;
+    }
+    public void setAmountInStock(int inputStock){
+        AmountInStock = inputStock;
+    }
+}
